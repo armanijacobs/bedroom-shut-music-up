42869304d4499280adda4b4c5e13909349887f4e
armanijacobs <armanipersonal@gmail.com>
2025-09-13 19:29:53 +0100

    design updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import bedroom3 from '../assets/bsm-bedroom3.png'
 import about from '../assets/about.png'
-import timeline from '../assets/timeline.png'
 
 const About = () => {
     return (
@@ -13,7 +12,7 @@ const About = () => {
                     className='w-2/5 h-2/5 py-2'
                     src={about}
                     alt="" />
-                <p className='text-white font-[montserrat] text-sm m-2 md:m-10 md:text-3xl'>This exhibition seeks to oﬀer an inter-generational reflection on the lived experiences of
+                <p className='text-white font-[Lato] text-sm m-2 md:m-10 md:text-3xl'>This exhibition seeks to oﬀer an inter-generational reflection on the lived experiences of
                     Afro/Caribbean descendants growing up in London’s multi-cultural environment
                     where we take a focused look into the area of Waltham Forest. This project aims
                     to explore how the early 2000’s marked a pivotal change for Afro-Caribbeans, from
@@ -22,9 +21,6 @@ const About = () => {
                     and what if anything has changed.</p>
             </div>
 
-            <img className='md:px-6 mt-6'
-                src={timeline}
-                alt="" />
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,23 @@ import bsm from '../assets/bsm-header1.png'
 
 const Header = () => {
     return (
-        <div
-            className='bg-primary'>
-            <motion.img
-                initial={{ opacity: 0, scale: 0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                    type: "spring",
-                    stiffness: 100,
-                    damping: 10,
-                    delay: 0.6,
-                }}
-                className='p-8'
-                src={bsm}
-                alt="" />
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 0.6,
+            }}
+            className='bg-primary px-8 md:px-18'>
 
-        </div>
+            <h1 className='font-[Bebas] tracking-wider text-white pt-10 md:pt-18 text-sm md:text-2xl lg:text-3xl text-center md:text-center'>Red Light Busking Presents</h1>
+            <h1 className='font-[Bebas] tracking-wider text-secondary text-5xl text-center md:text-center md:text-8xl lg:text-[180px]'>
+                Bedroom <span className='line-through'>Shut</span> Music Up
+            </h1>
+
+        </motion.div>
     )
 }
 
