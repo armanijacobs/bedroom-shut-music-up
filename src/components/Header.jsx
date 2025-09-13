424049e8bdd1d6fe9e819029b8ffe2e8d44c11e8
armanijacobs <armanipersonal@gmail.com>
2025-09-13 19:44:02 +0100

    header animation change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,33 @@ import bsm from '../assets/bsm-header1.png'
 
 const Header = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                type: "spring",
-                stiffness: 100,
-                damping: 10,
-                delay: 0.6,
-            }}
+        <div
             className='bg-primary px-8 md:px-18'>
 
-            <h1 className='font-[Bebas] tracking-wider text-white pt-10 md:pt-18 text-sm md:text-2xl lg:text-3xl text-center md:text-center'>Red Light Busking Presents</h1>
-            <h1 className='font-[Bebas] tracking-wider text-secondary text-5xl text-center md:text-center md:text-8xl lg:text-[180px]'>
+            <motion.h1
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 10,
+                    delay: 0.6,
+                }}
+                className='font-[Bebas] tracking-wider text-white pt-10 md:pt-18 text-sm md:text-2xl lg:text-3xl text-center md:text-center'>Red Light Busking Presents</motion.h1>
+            <motion.h1
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 10,
+                    delay: 0.6,
+                }}
+                className='font-[Bebas] tracking-wider text-secondary text-5xl text-center md:text-center md:text-8xl lg:text-[180px]'>
                 Bedroom <span className='line-through'>Shut</span> Music Up
-            </h1>
+            </motion.h1>
 
-        </motion.div>
+        </div>
     )
 }
 
