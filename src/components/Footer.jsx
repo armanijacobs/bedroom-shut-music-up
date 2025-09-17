69ba2f6fa2a7704c1989e0769532bcab39c43f38
armanijacobs <armanipersonal@gmail.com>
2025-09-17 14:13:09 +0100

    footer image removal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import redlight from '../assets/Red_Light_Neon_woman_FINAL.png'
 import trust from '../assets/English_Stamp_Black_PNG.png'
 
 
 const Footer = () => {
     return (
-        <div className='flex flex-row bg-secondary h-20 md:h-25 justify-around items-center px-25'>
+        <div className='flex flex-row bg-secondary h-20 md:h-25 justify-center space-x-7 items-center px-25'>
 
             <div>
                 <img className='h-10 md:h-15' src={trust} alt="" />
@@ -13,13 +12,9 @@ const Footer = () => {
             <div className='bg-secondary font-[montserrat] text-white flex flex-col justify-center text-center'>
                 <h5 className='font-bold text-[10px] underline'>Get in Touch</h5>
                 <p className='text-[10px] md:text-xs'>@redlightbuskin.</p>
-
                 <p className='text-[10px] md:text-xs'> Online Otters. All rights reserved.</p>
             </div>
 
-            <div>
-                <img className='h-16 md:h-24' src={redlight} alt="" />
-            </div>
         </div>
     )
 }
